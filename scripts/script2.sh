1205a82cbfed75779bfa3145fab36a6df8a011d7
MountainMan <46047927+MountainMan12@users.noreply.github.com>
2020-08-02 01:19:57 +0530

    Add files via upload

---
+++ scripts/script2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name='Ajay Sharma'
+email='abc@gmail.com'
+language='bash'
+biostack='genomics'
+username='@Ajay'
+
+echo $name','$email','$language','$biostack','$username
---
+++ scripts/script3.py
@@ -0,0 +1,8 @@
+#Python program to print data
+name='Kirti Sharma'
+email='xyz@gmail.com'
+language='python'
+biostacks='Data Analytics'
+username='Kirti'
+
+print(name+','+email+','+language+','+biostacks+','+username)
