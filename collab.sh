63023a001598e98bf7efec46452adc09b9b07959
MountainMan <46047927+MountainMan12@users.noreply.github.com>
2020-08-02 01:31:20 +0530

    Add files via upload

---
+++ collab.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git clone "https://github.com/MountainMan12/test-repo.git" "/home/$(whoami)/team-heisenberg"
+
+
+cd scripts/
+for file in $(ls);
+do
+	if [[ $file == *.py ]];
+	then
+		python $file >> ../details.csv
+	elif [[ $file == *.sh ]];
+	then
+		bash $file >> ../details.csv
+	elif [[ $file == *.R ]];
+	then
+		Rscript $file >> ../details.csv
+	fi
+done
